9e5d68d82319415e39e4a90406fcba9e38bb1ffe
RamirezR <robertramirez530@gmail.com>
2020-03-12 00:10:57 -0700

    wall bouncing

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
-export var xPos = 100
-export var yPos = 100
+export var xPos = 512
+export var yPos = 300
 
 export var deltaX = 5
 export var deltaY = 5
@@ -18,3 +18,8 @@ func _ready():
 func _process(delta):
 	position.x += deltaX
 	position.y += deltaY
+	
+	if position.y > 600 or position.y < 0:
+		deltaY = -deltaY
+	if position.x > 1024 or position.x < 0:
+		deltaX = -deltaX
